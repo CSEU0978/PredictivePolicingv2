e5dec3764eca5e93c64a20779fe6ac514d891526
CSEU0978 <e21cseu0978@bennett.edu.in>
2022-11-12 20:21:11 +0530

    Update ImportAndClean.py

--- ImportAndClean.py
+++ ImportAndClean.py
@@ -7,24 +7,20 @@ import pandas as pd
 
 
 def cleaning():
-    crimes = pd.read_csv("C:\\Users\\suran\\Desktop\\School\\1 UNIVERSITY\\BENNETT\\CSET211 Ai\\AI Hackathon\\Chicago_Crimes_2012_to_2017\\Chicago_Crimes_2012_to_2017_trimmed.csv")
+    crimes = pd.read_csv(r"C:\Users\suran\Desktop\School\1 UNIVERSITY\BENNETT\CSET204 Prob and Stats\Hackathon Predictive policing\Hackathon Predictive policing\Chicago_Crimes_2012_to_2017.csv\Chicago_Crimes_2012_to_2017.csv")
+    crimes.drop_duplicates(subset=['ID', 'Case Number'], inplace=True)
 
-    crimes=crimes.dropna(axis=0)
-    iucr_codes = ['1320', '810' , '820', '460', '480']
-    crimes_filt = crimes[crimes.IUCR.isin(iucr_codes)] 
+    crimes.Date = pd.to_datetime(crimes.Date, format='%m/%d/%Y %I:%M:%S %p',errors='ignore')
+    crimes.index = pd.DatetimeIndex(crimes.Date)
+    # print('Dataset Shape after drop_duplicate: ', crimes.shape)
+    crimes.drop(['Unnamed: 0', 'Case Number','Updated On','Year', 'FBI Code', 'Beat','Ward','Community Area', 'Location'], inplace=True, axis=1)
 
-    #print(crimes_filt.describe(include='all'))
-    # print('working')
-    print('Dataset Shape before drop_duplicate : ', crimes.shape)
-    #crimes.drop_duplicates(subset=['ID', 'Case Number'], inplace=True)
-    print('Dataset Shape after drop_duplicate: ', crimes.shape)
-    #crimes.drop(['Unnamed: 0', 'Case Number','Updated On','Year', 'FBI Code', 'Beat','Ward','Community Area', 'Location'], inplace=True, axis=1)
-    print(crimes_filt.head(5))
 
-    crimes_filt.Date = pd.to_datetime(crimes_filt.Date, format='%m/%d/%Y %I:%M:%S %p')
-    crimes_filt.index = pd.DatetimeIndex(crimes_filt.Date)
-    print(crimes.shape, "\n", crimes.info())
+    iucr_codes = ['1320', '0810' , '0820', '0460', '0480']
+    crimes = crimes[crimes['IUCR'].isin(iucr_codes)]
 
-    return crimes_filt
+    print(crimes.head(10))
+    print(crimes.info())
 
-cleaning()
+
+
