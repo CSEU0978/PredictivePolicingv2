79228c243cb69892e7b56889b8a3feddc04414d8
CSEU0978 <e21cseu0978@bennett.edu.in>
2022-11-12 18:17:40 +0530

    18h20 commit

--- ImportAndClean.py
+++ ImportAndClean.py
@@ -1,28 +1,29 @@
 
-import numpy as np 
-import pandas as pd 
 import matplotlib.pyplot as plt
-plt.style.use('seaborn')
-from sklearn.tree import DecisionTreeRegressor as dtr
+import numpy as np
+import pandas as pd
+
+#plt.style.use('seaborn')
+#from sklearn.tree import DecisionTreeRegressor as dtr
+
 
 def cleaning():
-    crimes = pd.read_csv(r"C:\Users\suran\Desktop\School\1 UNIVERSITY\BENNETT\CSET204 Prob and Stats\Hackathon Predictive policing\Hackathon Predictive policing\Chicago_Crimes_2012_to_2017.csv\Chicago_Crimes_2012_to_2017.csv")
+    crimes = pd.read_csv("C:\\Users\\suran\\Desktop\\School\\1 UNIVERSITY\\BENNETT\\CSET211 Ai\\AI Hackathon\\Chicago_Crimes_2012_to_2017\\Chicago_Crimes_2012_to_2017_trimmed.csv")
 
     crimes=crimes.dropna(axis=0)
     iucr_codes = ['1320', '810' , '820', '460', '480']
     crimes_filt = crimes[crimes.IUCR.isin(iucr_codes)] 
 
-    print(crimes_filt.describe(include='all'))
+    #print(crimes_filt.describe(include='all'))
     # print('working')
     # print('Dataset Shape before drop_duplicate : ', crimes.shape)
-    crimes.drop_duplicates(subset=['ID', 'Case Number'], inplace=True)
+    #crimes.drop_duplicates(subset=['ID', 'Case Number'], inplace=True)
     # print('Dataset Shape after drop_duplicate: ', crimes.shape)
     #crimes.drop(['Unnamed: 0', 'Case Number','Updated On','Year', 'FBI Code', 'Beat','Ward','Community Area', 'Location'], inplace=True, axis=1)
-    # print(crimes.head(3))
+    print(crimes_filt.head(5))
 
-    crimes.Date = pd.to_datetime(crimes.Date, format='%m/%d/%Y %I:%M:%S %p')
-    crimes.index = pd.DatetimeIndex(crimes.Date)
+    #0crimes_filt.Date = pd.to_datetime(crimes_filt.Date, format='%m/%d/%Y %I:%M:%S %p')
+    #crimes_filt.index = pd.DatetimeIndex(crimes_filt.Date)
     # print(crimes.shape, "\n", crimes.info())
 
-    return crimes
-
+    return crimes_filt
--- trendsgraph.py
+++ trendsgraph.py
@@ -1,25 +1,26 @@
 import numpy as np 
 import pandas as pd 
 import matplotlib.pyplot as plt
-plt.style.use('seaborn')
+#plt.style.use('seaborn')
+import seaborn as sns
 import ImportAndClean
 
 def trends():
-    crimes = pd.read_csv(r"C:\Users\suran\Desktop\School\1 UNIVERSITY\BENNETT\CSET204 Prob and Stats\Hackathon Predictive policing\Hackathon Predictive policing\Chicago_Crimes_2012_to_2017.csv\Chicago_Crimes_2012_to_2017.csv")
-
+    #crimes = pd.read_csv(r"C:\Users\suran\Desktop\School\1 UNIVERSITY\BENNETT\CSET204 Prob and Stats\Hackathon Predictive policing\Hackathon Predictive policing\Chicago_Crimes_2012_to_2017.csv\Chicago_Crimes_2012_to_2017.csv")
+    #crimes = ImportAndClean.cleaning()
     # print('working')
     # print('Dataset Shape before drop_duplicate : ', crimes.shape)
-    crimes.drop_duplicates(subset=['ID', 'Case Number'], inplace=True)
+    #crimes.drop_duplicates(subset=['ID', 'Case Number'], inplace=True)
     # print('Dataset Shape after drop_duplicate: ', crimes.shape)
 
-    crimes.drop(['Unnamed: 0', 'Case Number','Updated On','Year', 'FBI Code', 'Beat','Ward','Community Area', 'Location'], inplace=True, axis=1)
+    #crimes.drop(['Unnamed: 0', 'Case Number','Updated On','Year', 'FBI Code', 'Beat','Ward','Community Area', 'Location'], inplace=True, axis=1)
 
     # print(crimes.head(3))
 
-    crimes.Date = pd.to_datetime(crimes.Date, format='%m/%d/%Y %I:%M:%S %p')
-    crimes.index = pd.DatetimeIndex(crimes.Date)
+    #crimes.Date = pd.to_datetime(crimes.Date, format='%m/%d/%Y %H:%M %p')
+    #crimes.index = pd.DatetimeIndex(crimes.Date)
 
-    print(crimes.shape, "\n", crimes.info())
+    #print(crimes.shape, "\n", crimes.info())
 
  # Visualisation
 
@@ -27,10 +28,13 @@ def trends():
 # IUCR_data = crimes.query('IUCR  == "0820" or IUCR == "0810"') # Theft
 # IUCR_data = crimes.query('IUCR  == "0460" or IUCR == "0486"') # Battery
 # IUCR_data = crimes.query('IUCR  == "1320"') # Criminla Damage
-    data_feed = ['IUCR  == "0820" or IUCR == "0810"', 'IUCR  == "0460" or IUCR == "0486"', 'IUCR  == "1320"']
+    #data_feed = ['IUCR  == "0820" or IUCR == "0810"', 'IUCR  == "0460" or IUCR == "0486"', 'IUCR  == "1320"']
+    #data_feed = ImportAndClean.crimes_filt()
+    crimes = ImportAndClean.cleaning()
     crime_type = ["Theft", "Battery", "Criminal Damage"]
     for i in range(0, 3):
-        IUCR_data = crimes.query(data_feed[i])
+        #IUCR_data = crimes.query(data_feed[i])
+        IUCR_data = crimes.query(crimes[i])
         plt.figure(figsize=(11,5))
         IUCR_data.resample('M').size().plot(legend=False)
         plt.title('Number of {} per month (2012 - 2017)'.format(crime_type[i]))
